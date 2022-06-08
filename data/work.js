b6cfdd3ff5ddf629a688c90c4f6ee455d03e6cd0
psison <sisonpatrick29@yahoo.com>
2022-06-08 08:31:00 -0500

    create sticky nav bar

--- data/work.js
+++ data/work.js
@@ -1,9 +1,9 @@
 const projects = [
     {
-        'title': 'Tic-Tac-Toe',
-        'image': '',
-        'description': 'My rendition of the classic game, Tic-Tac-Toe!',
-        'githubLink': 'https://github.com/psison92/ttt-weekend',
-        'deploymentLink': 'https://ttt-patrick-s.netlify.app/'
+        title: 'Tic-Tac-Toe',
+        image: '',
+        description: 'My rendition of the classic game, Tic-Tac-Toe!',
+        github: 'https://github.com/psison92/ttt-weekend',
+        deployment: 'https://ttt-patrick-s.netlify.app/'
     }
 ]
