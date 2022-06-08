5bbb5c8309b4b7d598753cf792b0e4ed104f9f8d
psison <sisonpatrick29@yahoo.com>
2022-06-08 08:51:46 -0500

    add tic-tac-toe image

--- data/work.js
+++ data/work.js
@@ -1,7 +1,7 @@
 const projects = [
     {
         title: 'Tic-Tac-Toe',
-        image: '',
+        image: '/assets/tic-tac-toe.jpg',
         description: 'My rendition of the classic game, Tic-Tac-Toe!',
         github: 'https://github.com/psison92/ttt-weekend',
         deployment: 'https://ttt-patrick-s.netlify.app/'
