9a6aaa075b8268b607dce3329bed329764eb1631
psison <sisonpatrick29@yahoo.com>
2022-07-21 21:31:27 -0500

    add stranger drinks app data

--- data/work.js
+++ data/work.js
@@ -12,6 +12,13 @@ const projects = [
         description: 'The social network for gym enthusiasts.',
         github: 'https://github.com/psison92/fitIn',
         deployment: 'https://fit-in.herokuapp.com/'
+    },
+    {
+        title: 'Stranger Drinks',
+        image: 'https://user-images.githubusercontent.com/104323543/180257526-2f97bcbd-0eaf-45d5-ab93-de7c3b36fc5a.png',
+        description: 'Stranger Things themed app where users create and share drink recipes.',
+        github: 'https://github.com/psison92/stranger-drinks-front-end',
+        deployment: 'https://stranger-drinks.netlify.app/'
     }
 ]
 
