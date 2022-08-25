7d365db6e6043ab5e8fe1c2a480680bcd3c3a3a2
psison <sisonpatrick29@yahoo.com>
2022-08-24 20:25:16 -0500

    fix work card styling

--- data/work.js
+++ data/work.js
@@ -1,28 +1,28 @@
 const projects = [
     {
         title: 'Tic-Tac-Toe',
-        image: '/assets/images/tic-tac-toe.jpg',
+        image: '/assets/images/tic-tac-toe.png',
         description: 'The classic game, with a little flair.',
         github: 'https://github.com/psison92/ttt-weekend',
         deployment: 'https://ttt-patrick-s.netlify.app/'
     },
     {
         title: 'FitIn',
-        image: '/assets/images/fit-in-screenshot.jpg',
+        image: '/assets/images/fit-in.png',
         description: 'The social network for gym enthusiasts.',
         github: 'https://github.com/psison92/fitIn',
         deployment: 'https://fit-in.herokuapp.com/'
     },
     {
         title: 'Stranger Drinks',
-        image: 'https://user-images.githubusercontent.com/104323543/180257526-2f97bcbd-0eaf-45d5-ab93-de7c3b36fc5a.png',
+        image: '/assets/images/stranger-drinks.png',
         description: 'Stranger Things themed app where users create and share drink recipes.',
         github: 'https://github.com/psison92/stranger-drinks-front-end',
         deployment: 'https://stranger-drinks.netlify.app/'
     },
     {
         title: 'Tupperwhere',
-        image: '/assets/images/landing.png',
+        image: '/assets/images/tupperwhere.png',
         description: 'Never forget about your leftovers again!',
         github: 'https://github.com/psison92/tupperwhere',
         deployment: 'https://tupperwhere.herokuapp.com/'
